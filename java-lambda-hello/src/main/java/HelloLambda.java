d73be19819b71e7b07fa5409d9bba74c673caaed
deepextractor <deep.extractor@gmail.com>
2025-05-08 01:36:02 +0530

    java-lambda-sum:new_changes java-lambda-hello:new_changes

--- java-lambda-hello/src/main/java/HelloLambda.java
+++ java-lambda-hello/src/main/java/HelloLambda.java
@@ -1,6 +1,6 @@
 public class HelloLambda {
     public static void main(String[] args) {
-        Runnable hello = () -> System.out.println("Hello,  World!");
+        Runnable hello = () -> System.out.println("Hello, World!");
         hello.run();
     }
 }
