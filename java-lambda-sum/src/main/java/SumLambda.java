e8304549db76d1a26be013591bf3ddc639cef550
deepextractor <deep.extractor@gmail.com>
2025-05-08 01:13:14 +0530

    java-lambda-sum:new_changes java-lambda-hello:new_changes

--- java-lambda-sum/src/main/java/SumLambda.java
+++ java-lambda-sum/src/main/java/SumLambda.java
@@ -9,7 +9,7 @@ public class SumLambda {
             double num1 = Double.parseDouble(args[0]);
             double num2 = Double.parseDouble(args[1]);
             double sum = num1 + num2;
-            System.out.println("The sum is:" + sum);
+            System.out.println("The sum is :" + sum);
         } catch (NumberFormatException e) {
             System.out.println("Invalid input. Please enter valid numbers.");
         }
