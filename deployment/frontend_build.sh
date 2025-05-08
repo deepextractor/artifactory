4b42cdbef3302c910b22d54d7ce0ad6372e00524
deepextractor <deep.extractor@gmail.com>
2025-05-09 01:29:44 +0530

    frontend: building frontend

---
+++ deployment/frontend_build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+FRONTEND_VERSION="1.0.0"
+PACKAGE_NAME=ExtractorFrontend-${FRONTEND_VERSION}.zip
+ARTIFACTORY_URL="https://maven.pkg.github.com/deepextractor/"
+ARTIFACTORY_REPO="artifactory"
+ARTIFACT_PATH="com/github/deepextractor/${PACKAGE_NAME}"
+ARTIFACTORY_MAVEN_USER="deepextractor"
+ARTIFACTORY_MAVEN_PASS="${GITHUB_TOKEN}"
+
+artifact_url="${ARTIFACTORY_URL}/${ARTIFACTORY_REPO}/${ARTIFACT_PATH}"
+echo "Checking if following artifact is available: ${artifact_url}"
+response_code=$(curl -s -o /dev/null -w "%{http_code}" --user $ARTIFACTORY_MAVEN_USER:$ARTIFACTORY_MAVEN_PASS -X HEAD "${artifact_url}")
+echo $response_code
+
+set -e # Enable errexit
+# function run_sonar(){
+#     set +e
+#     echo "----------------------------------- Running Sonar ----------------------------"
+#     npm install sonar-scanner
+#     npm run sonar
+#     set -e
+# }
+
+if [[ "${response_code}" -eq 200 ]]; then
+    echo "Current frontend version exist in Artifactory. Skipping Frontend Build"
+else
+    echo "Artifact does not exist in Artifactory: Starting build"
+#    run_sonar
+    echo "---------------------------- Starting Frontend Build ----------------------------"
+    npm ci --cache .npm --prefer-offline
+    npm run build
+    zip -q -r $PACKAGE_NAME ./dist/frontend/*
+    curl -X PUT --user $ARTIFACTORY_MAVEN_USER:$ARTIFACTORY_MAVEN_PASS -T $PACKAGE_NAME "https://maven.pkg.github.com/deepextractor/artifactory/com/github/deepextractor/ui/${PACKAGE_NAME}"
+fi
