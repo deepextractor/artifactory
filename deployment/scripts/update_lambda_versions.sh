618313c9847e31d326f25cf292f480f84f79ca30
deepextractor <deep.extractor@gmail.com>
2025-05-16 00:45:05 +0530

    java-lambda-hello java-lambda-sum

--- deployment/scripts/update_lambda_versions.sh
+++ deployment/scripts/update_lambda_versions.sh
@@ -3,11 +3,11 @@
 LAMBDA_ARTIFACT=$1
 JAVA_LAMBDA_VERSION=$2
 
-echo "${LAMBDA_ARTIFACT}=${JAVA_LAMBDA_VERSION}" >> lambda_versions.txt
+echo "${LAMBDA_ARTIFACT}=${JAVA_LAMBDA_VERSION}" >> deployment/versions/${LAMBDA_ARTIFACT}.txt
 
 git config --global user.name "github-actions[bot]"
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 
-git add lambda_versions.txt
-git commit -m "Update lambda_versions.txt [skip ci]" || echo "No changes to commit"
+git add deployment/versions/${LAMBDA_ARTIFACT}.txt
+git commit -m "Update ${LAMBDA_ARTIFACT}.txt [skip ci]" || echo "No changes to commit"
 git push
